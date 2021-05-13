23b77f8701784a98b5da9f8d0b9e9d36d5c61eda
Joao-Gontijo <jvgontijo166@gmail.com>
2021-05-13 17:11:59 -0300

    Componentes - Cliente e Produto
    Aprendendo a criar componentes

---
+++ src/components/Cliente.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id=cliente>
+        <h2 id=cliente-nome>Cliente aqui - Exemplo Componente</h2>
+        <h3>Descrição do cliente: text text text</h3>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+    #cliente{
+        color: green;
+        background-color: black;
+    }
+    #cliente-nome{
+        color: aliceblue;
+    }
+</style>
+
